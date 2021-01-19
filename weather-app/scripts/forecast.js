9d7d9aa045452179224124a4bb8d2ed232c39393
luckystoned <luckystoned.dev@gmail.com>
2021-01-18 21:10:05 -0300

    Weather App: forecast get city get weather

--- weather-app/scripts/forecast.js
+++ weather-app/scripts/forecast.js
@@ -1,5 +1,17 @@
 const key = '8OPaQDOJ2AV9iAfDFgHt1ntIKQ27UsNG';
 
+//get weather information
+const getWeather =  async (id) => {
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+}
+
+//get city information
 const getCity = async (city) => {
     const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
     const query = `?apikey=${key}&q=${city}`;
@@ -10,6 +22,10 @@ const getCity = async (city) => {
     return data[0];
 }
 
-getCity('manchester')
-    .then(data => console.log(data))
-    .catch(err => console.log(err));
+getCity('barcelona')
+    .then(data => {
+        return getWeather(data.Key);
+    }).then(data => {
+        console.log(data);
+    })
+.catch(err => console.log(err));
