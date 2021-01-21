96902328ab3e1225df54c7ff7ee1681981927fba
luckystoned <luckystoned.dev@gmail.com>
2021-01-21 03:15:39 -0300

    Weather Icons & Images

--- weather-app/scripts/app.js
+++ weather-app/scripts/app.js
@@ -2,6 +2,8 @@
 const cityForm = document.querySelector('form');
 const card = document.querySelector('.card');
 const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
 
 const updateUI= data => {
 
@@ -11,7 +13,7 @@ const updateUI= data => {
 
     //desctructure propierties
     const {cityDets, weather} = data;
-    
+
     //update details template
     details.innerHTML = `
         <h5 class="my-3">${cityDets.EnglishName}</h5>
@@ -21,6 +23,20 @@ const updateUI= data => {
             <span>&deg;C</span>
         </div>
     `
+
+    //update the nigth/day & icon images
+
+    const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+    icon.setAttribute('src', iconSrc);
+
+    let timeSrc = null;
+    if(weather.IsDayTime){
+        timeSrc = 'img/day.svg'
+    } else {
+        timeSrc = 'img/night.svg'
+    }
+
+    time.setAttribute('src', timeSrc);
 
     //remove th d-none class if present
     if(card.classList.contains('d-none')){
