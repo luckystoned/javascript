5d8dc3b0ac91e850449e58132b871ab301e71c84
luckystoned <luckystoned.dev@gmail.com>
2021-01-21 02:42:12 -0300

    update the location, update the ui

--- weather-app/scripts/forecast.js
+++ weather-app/scripts/forecast.js
@@ -1,31 +1,37 @@
 const key = '8OPaQDOJ2AV9iAfDFgHt1ntIKQ27UsNG';
 
 //get weather information
-const getWeather =  async (id) => {
+const getWeather =  async id => {
     const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
     const query = `${id}?apikey=${key}`;
 
     const response = await fetch(base + query);
     const data = await response.json();
 
+    console.log('getWeather', data)
+
     return data[0];
 }
 
 //get city information
-const getCity = async (city) => {
+const getCity = async city => {
     const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
     const query = `?apikey=${key}&q=${city}`;
 
     const response = await fetch(base + query);
     const data = await response.json();
 
+    console.log('getCity', data)
+
     return data[0];
 }
 
-getCity('barcelona')
-    .then(data => {
-        return getWeather(data.Key);
-    }).then(data => {
-        console.log(data);
-    })
-.catch(err => console.log(err));
+
+// Harcoding script
+// getCity('manchester')
+//     .then(data => {
+//         return getWeather(data.Key);
+//     }).then(data => {
+//         console.log(data);
+//     })
+// .catch(err => console.log(err));
